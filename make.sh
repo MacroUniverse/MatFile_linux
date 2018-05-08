4c80b59f10c5cc5e73ecd8d62f02d79679d71173
MacroUniverse <270174408@qq.com>
2018-05-08 12:11:00 -0400

    now portable
    all *.so files included, this project does not require Matlab installation any more

--- make.sh
+++ make.sh
@@ -1,5 +1,3 @@
-#LD_LIBRARY_PATH="/usr/local/MATLAB/R2017a/bin/glnxa64"
-g++ -std=c++11 mattest.cpp matsave.cpp -o mattest.x -Wl,-rpath-link,/usr/local/MATLAB/R2017a/bin/glnxa64 -L/usr/local/MATLAB/R2017a/bin/glnxa64 -l mat -l mx
-#g++ -std=c++11 mattest.cpp -L/usr/local/MATLAB/R2017a/bin/glnxa64 -l mat -l mx -l mwfl -l mwfoundation_usm -l mwi18n -l ut 
+g++ -std=c++11 mattest.cpp matsave.cpp -o mattest.x -Wl,-rpath-link,./bin -L./bin -l mat -l mx
 
-LD_LIBRARY_PATH="/usr/local/MATLAB/R2017a/bin/glnxa64" ./mattest.x
+LD_LIBRARY_PATH="./bin" ./mattest.x
